1a25926c198f7b91bf0e711303c343e5ec62dcd4
Louise <louise.cch@gmail.com>
2025-10-30 20:20:05 +0000

    revert: remove Vercel serverless proxy implementation
    This reverts commit 699468f (feat: secure Strapi API token with Vercel serverless proxy).
    
    Reason: Rolling back to direct API implementation approach.

--- client/src/makeRequest.js
+++ client/src/makeRequest.js
@@ -1,32 +1,8 @@
 import axios from "axios";
 
-const USE_PROXY =
-  process.env.REACT_APP_USE_PROXY === "true" ||
-  process.env.NODE_ENV === "production";
-
 export const makeRequest = axios.create({
-  baseURL: USE_PROXY ? "/api/strapi-proxy" : process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
-    "Content-Type": "application/json",
+    Authorization: "bearer " + process.env.REACT_APP_API_TOKEN,
   },
 });
-
-makeRequest.interceptors.request.use(
-  (config) => {
-    if (USE_PROXY) {
-      const url = config.url || "";
-      config.params = {
-        ...config.params,
-        path: url,
-      };
-      config.url = "";
-    } else {
-      config.headers.Authorization =
-        "bearer " + process.env.REACT_APP_API_TOKEN;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
