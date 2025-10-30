8bb0a4fc665ed6aed1dcb0a6e8baf977fc3fa998
Louise <louise.cch@gmail.com>
2025-10-30 16:59:58 +0000

    refactor: remove br tags and use semantic HTML in Stores

--- client/src/pages/Stores/Stores.jsx
+++ client/src/pages/Stores/Stores.jsx
@@ -2,34 +2,33 @@ import React from "react";
 import "./stores.scss";
 
 const Stores = () => {
+  const stores = [
+    {
+      address: "147 Shoreditch High St",
+      city: "London E1 6JE",
+      phone: "+44 (0) 20 3848 8860",
+    },
+    {
+      address: "116 Regent's Park Rd",
+      city: "London NW1 8UR",
+      phone: "+44 (0) 20 7253 1644",
+    },
+    {
+      address: "52 Rochester Walk",
+      city: "London SE1 9AF",
+      phone: "+44 (0) 20 8617 8760",
+    },
+  ];
+
   return (
     <div className="containerStore fade-in">
-      <span></span>
-      <div className="shop">
-        147 Shoreditch High St,
-        <br />
-        London E1 6JE
-        <br />
-        <span>+44 (0) 20 3848 8860</span>
-        <br />
-      </div>
-      <div className="shop">
-        116 Regent's Park Rd,
-        <br />
-        London NW1 8UR
-        <br />
-        <span> +44 (0) 20 7253 1644</span>
-        <br />
-      </div>
-      <div className="shop">
-        52 Rochester Walk
-        <br />
-        London SE1 9AF
-        <br />
-        <span> +44 (0) 20 8617 8760</span>
-        <br />
-      </div>
-      <span></span>
+      {stores.map((store, index) => (
+        <div key={index} className="shop">
+          <p className="address">{store.address}</p>
+          <p className="city">{store.city}</p>
+          <p className="phone">{store.phone}</p>
+        </div>
+      ))}
     </div>
   );
 };
