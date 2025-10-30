0240204135c86bff9d4d5a705323f9a77d584699
Louise <louise.cch@gmail.com>
2025-10-30 19:25:16 +0000

    refactor: use environment variable for API URL instead of hardcoded value

--- client/src/constant.js
+++ client/src/constant.js
@@ -1,4 +1,4 @@
 export const AVATAR_API = "https://ui-avatars.com/api";
-export const API = "https://strapi-production-a8c7.up.railway.app/api";
+export const API = process.env.REACT_APP_API_URL;
 export const AUTH_TOKEN = "authToken";
 export const BEARER = "Bearer";
