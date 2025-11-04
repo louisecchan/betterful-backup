f063d40ec66a42c6a1a63bb86e04a90d39d96e18
Louise <louise.cch@gmail.com>
2025-11-04 00:45:35 +0000

    fix: move CORS middleware before errors to fix missing Allow-Origin header

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,19 +1,4 @@
 module.exports = [
-  "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
   {
     name: "strapi::cors",
     config: {
@@ -30,10 +15,26 @@ module.exports = [
       credentials: true,
     },
   },
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   "strapi::poweredBy",
   "strapi::logger",
   "strapi::query",
   "strapi::body",
   "strapi::session",
+  "strapi::favicon",
   "strapi::public",
 ];
