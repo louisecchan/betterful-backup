a786b24b56dbb2b300edf6e8aba3ba40ab463492
unknown <louise.cch@gmail.com>
2026-03-21 13:11:34 +0000

    refactor(order): move stripe logic from controller to service

--- backend/src/api/order/services/order.js
+++ backend/src/api/order/services/order.js
@@ -1,9 +1,44 @@
-("use strict");
+'use strict';
 
 /**
  * order service
  */
 
-const { createCoreService } = require("@strapi/strapi").factories;
+const stripe = require('stripe')(process.env.STRIPE_KEY);
+const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService("api::order.order");
+module.exports = createCoreService('api::order.order', ({ strapi }) => ({
+  async createCheckoutSession(products) {
+    const lineItems = await Promise.all(
+      products.map(async (product) => {
+        const item = await strapi
+          .service('api::product.product')
+          .findOne(product.id);
+
+        return {
+          price_data: {
+            currency: 'usd',
+            product_data: { name: item.title },
+            unit_amount: Math.round(item.price * 100),
+          },
+          quantity: product.quantity,
+        };
+      })
+    );
+
+    const session = await stripe.checkout.sessions.create({
+      shipping_address_collection: { allowed_countries: ['US', 'CA'] },
+      payment_method_types: ['card'],
+      mode: 'payment',
+      success_url: `${process.env.CLIENT_URL}?success=true`,
+      cancel_url: `${process.env.CLIENT_URL}?success=false`,
+      line_items: lineItems,
+    });
+
+    await strapi
+      .service('api::order.order')
+      .create({ data: { products, stripeId: session.id } });
+
+    return session;
+  },
+}));
